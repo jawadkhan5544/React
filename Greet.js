e0a016025d1063f9d1f17ed86527361bf1e0ab5a
jawadkhan5544 <134105520+jawadkhan5544@users.noreply.github.com>
2023-05-21 01:33:23 +0100

    destruting of ftn comp

---
+++ Greet.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// function Greet(){
+//     return <h1>Hello this is Statless Function Components</h1>
+// 
+const Greet= ({name, Group})=>{
+
+return (
+    <div> <h1>Hello this is stateless ftn using props {name} {Group}</h1> 
+  
+
+</div>
+)
+}
+export default Greet
